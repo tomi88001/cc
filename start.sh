43367010177431aecd362206c946da9911137cf8
tm <tm886001@gmail.com>
2025-04-05 18:17:09 +0100

    1

--- start.sh
+++ start.sh
@@ -4,14 +4,13 @@ git pull
 # 运行 Python 脚本，并后台执行
 PYTHON_SCRIPT="/root/cc/main.py"
 sh stop.sh
-if [ "$1" = "back" ]; then
+if [ "$4" = "back" ]; then
     if [ ! -d "$PYTHON_SCRIPT" ]; then
-        python3 "$PYTHON_SCRIPT" "cc" "$1" $2 $3
-        # nohup python3 "$PYTHON_SCRIPT" "cc" "$1" "$2" "$3" > output.log 2>&1 &
+        nohup python3 "$PYTHON_SCRIPT" "cc" "$1" "$2" "$3" > output.log 2>&1 &
         tail -f output.log
     fi
 else
-    python3 "$PYTHON_SCRIPT"
+    python3 "$PYTHON_SCRIPT" "cc" "$1" $2 $3
 fi
 
 
